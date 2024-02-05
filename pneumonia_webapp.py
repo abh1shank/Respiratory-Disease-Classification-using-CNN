d7afe09017ebdd66f8086238ce767d50539cf53e
Abhishank Saran <97939389+abh1shank@users.noreply.github.com>
2024-02-05 23:19:39 +0530

    Add files via upload

--- pneumonia_webapp.py
+++ pneumonia_webapp.py
@@ -16,7 +16,7 @@ st.header('Please upload a chest X-ray image')
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-model = load_model('https://github.com/abh1shank/Respiratory-Disease-Classification-using-CNN/blob/main/Pnumonia_model.h5')
+model = load_model('./Pnumonia_model.h5')
 
 # load class names
 class_names=['Unhealthy','Healthy']
