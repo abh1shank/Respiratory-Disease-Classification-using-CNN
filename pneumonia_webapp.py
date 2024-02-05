5a061863f5a2ab1e35b51edd0988a72d80ee5a7c
Abhishank Saran <97939389+abh1shank@users.noreply.github.com>
2024-02-05 20:29:52 +0530

    Add files via upload

---
+++ pneumonia_webapp.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+from util import classify, set_background
+
+
+
+# set title
+st.title('Pneumonia classification')
+
+# set header
+st.header('Please upload a chest X-ray image')
+
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# load classifier
+model = load_model('C:/Users/saran/Downloads/Pnumonia_model.h5')
+
+# load class names
+class_names=['Unhealthy','Healthy']
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
