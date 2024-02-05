b7b7bbb3f5089eaa7d79ad19f97fb59730e112d8
Abhishank Saran <97939389+abh1shank@users.noreply.github.com>
2024-02-05 23:13:02 +0530

    Add files via upload

--- pneumonia_webapp.py
+++ pneumonia_webapp.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from PIL import Image
 import numpy as np
-
 from util import classify, set_background
 
 
@@ -17,7 +16,7 @@ st.header('Please upload a chest X-ray image')
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-model = load_model('C:/Users/saran/Downloads/Pnumonia_model.h5')
+model = load_model('https://github.com/abh1shank/Respiratory-Disease-Classification-using-CNN/blob/main/Pnumonia_model.h5')
 
 # load class names
 class_names=['Unhealthy','Healthy']
